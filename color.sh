61d644f172c0c1e5b8a93dc19e2eedbb18946a3c
chengjp <aorangeaorange@sina.com>
2019-03-13 11:01:03 +0800

    ...

---
+++ color.sh
@@ -0,0 +1,24 @@
+bg_color_prefix="\\033[";
+# font color
+font_black="30";#black
+font_red="31";#red
+font_green="32";#green
+font_yellow="33";#yellow
+font_blue="34";#blue
+font_purple="35";#purple
+font_darkgreen="36";#darkgreen
+font_white="37";#white
+
+#background color
+bg_black="40";
+bg_deepred="41";
+bg_green="42";
+bg_yellow="43";
+bg_blue="44";
+bg_purple="45";
+bg_darkgreen="46";
+bg_white="47";
+
+#exmaple
+#echo "${bg_color_prefix}${bg_black};${font_darkgreen}m test123 字颜色测试"
+
---
+++ import.sh
@@ -0,0 +1,3 @@
+source ./color.sh
+#!/bin/zsh
+echo "${bg_color_prefix}${bg_black};${font_darkgreen}m import字颜色测试"  
